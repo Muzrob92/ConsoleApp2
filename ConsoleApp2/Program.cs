d31c980a403c1adbcefd29bc0ca2fa247498155d
Mustafojon <Mustafojon@DESKTOP-P6KNFPB>
2024-12-03 12:48:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int num1 = Int32.MaxValue;
+        Console.WriteLine("Multiplication of {0}x{0} without Math function - {1}", num1, num1 * num1);
+        Console.WriteLine("Multiplication of {0}x{0} by Math BigMul function - {1}", num1, Math.BigMul(num1, num1));
+    }
+}
